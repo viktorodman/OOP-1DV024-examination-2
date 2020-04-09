fb4092f06bb354d5e70fdce121723e764d7c08df
Viktor Ödman <vo222dq@student.lnu.se>
2020-04-09 22:29:13 +0200

    Adding methods and properties for Shape2D

--- FigurativtArv/Shape.cs
+++ FigurativtArv/Shape.cs
@@ -2,22 +2,34 @@ using System;
 
 namespace FigurativtArv
 {
+    /// <summary>
+    /// Represents a shape
+    /// </summary>
     public abstract class Shape
     {
+        /// <summary>
+        /// Returns true if the shape is a 3d shape
+        /// </summary>
+        /// <value></value>
         public bool Is3D
         {
             get => ((int)ShapeType == 1) ? false : true;
         }
+        /// <summary>
+        /// Gets and sets the Shape Type.
+        /// </summary>
+        /// <value></value>
         public ShapeType ShapeType {get; private set;}
-
+        /// <summary>
+        /// Initial
+        /// </summary>
+        /// <param name="shapeType"></param>        
         protected Shape(ShapeType shapeType)
         {
             ShapeType = shapeType;
         }
 
-        public abstract string ToString(string format)
-        {
-            throw new NotImplementedException();
-        }
+        public virtual string ToString(string format) => $"ShapeType: {ShapeType} Is3D: {Is3D}";
+        
     }
 }
--- FigurativtArv/Shape2D.cs
+++ FigurativtArv/Shape2D.cs
@@ -1,7 +1,65 @@
+using System;
+
 namespace FigurativtArv
 {
-    public class Shape2D
+    public abstract class Shape2D : Shape
     {
+        private double _length;
+        private double _width;
+
+        public abstract double Area { get;}
         
+        public abstract double Perimeter { get;}
+        public double Length
+        {
+            get => _length;
+
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value must be greater than 0");
+                }
+                _length = value;
+            }
+        }
+         public double Width
+        {
+            get => _width;
+
+            set 
+            { 
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value must be greater than 0");
+                }
+                _width = value;
+            }
+        }
+
+        protected Shape2D(ShapeType shapeType, double length, double width)
+            :base(shapeType)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override string ToString(string format)
+        {
+            string str;
+
+            if(format == "G" || string.IsNullOrEmpty(format))
+            {
+                str = $"Längd : {_length}\nBredd : {_width}\nOmkrets : {Perimeter}\nArea : {Area}\n";
+            } else if (format == "R")
+            {
+                str = $"{ShapeType} {_length} {_width} {Perimeter} {Area}";
+            } else {
+                throw new FormatException("No a correct format");
+            }
+            return str;
+        }
+
+        public override string ToString() => $"{ShapeType} {_length} {_width} {Perimeter} {Area}";
     }
 }
