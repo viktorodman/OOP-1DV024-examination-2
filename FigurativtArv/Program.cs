d63744ad880bf1c1aa4e139ace453de31471dab1
Viktor Ödman <vo222dq@student.lnu.se>
2020-04-09 15:38:19 +0200

    Adding console app FigurativtArv

---
+++ FigurativtArv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FigurativtArv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
